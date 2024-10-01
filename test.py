de0beeae93a49206c5a2cefa20cade4941ae7e22
aibozzz <zhanhongxu499@gmail.com>
2024-10-01 13:53:03 +0800

    111

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 def plus(x,y):
     return x + y
 
-inp = input()
-x,y = inp.split(',')
-x = int(x)
-y = int(y)
-print(plus(x,y))
+
 
 if __name__ == "__main__":
     inp = input()
--- test2.py
+++ test2.py
@@ -1,3 +1,3 @@
-from test import *
+from test import plus
 if __name__ == "__main__":
     print(plus(x,y))
